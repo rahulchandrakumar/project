ba498dff5d9ad19b422e27b3fe1e7dc09a80af91
rahulchandrakumar <157479239+rahulchandrakumar@users.noreply.github.com>
2024-08-08 15:30:42 +0530

    Update and rename source.java to Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+// Program.cs
+using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // Add a simple calculation
+            int a = 5;
+            int b = 10;
+            int sum = a + b;
+            Console.WriteLine($"The sum of {a} and {b} is {sum}");
+        }
+    }
+}
